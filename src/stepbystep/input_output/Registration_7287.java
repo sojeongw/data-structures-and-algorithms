11d177bec9da31bd853a39f573d2f53673c6d7ea
sojeongw <redthink90@gmail.com>
2019-08-28 14:54:54 +0900

    Step 1: 입출력과 사칙연산

---
+++ src/stepbystep/input_output/Registration_7287.java
@@ -0,0 +1,8 @@
+package stepbystep.input_output;
+
+public class Registration_7287 {
+	public static void main(String[] args) {
+	     System.out.println("7\n"+                      
+	                        "id");                          
+	    }
+}
