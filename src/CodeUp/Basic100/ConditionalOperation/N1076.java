d408d24419b2b70813ded6edd561f90ff6c20092
sojeongw <redthink90@gmail.com>
2019-09-26 14:30:55 +0900

    [CodeUp] 논리연산, 조건문, 반복문

---
+++ src/CodeUp/Basic100/ConditionalOperation/N1076.java
@@ -0,0 +1,16 @@
+package CodeUp.Basic100.ConditionalOperation;
+
+import java.util.Scanner;
+
+public class N1076 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char a = sc.next().charAt(0);
+        char t = 'a';
+
+        while(t<=a){
+            System.out.print(t + " ");
+            t++;
+        }
+    }
+}
---
+++ src/CodeUp/Basic100/ConditionalOperation/N1077.java
@@ -0,0 +1,16 @@
+package CodeUp.Basic100.ConditionalOperation;
+
+import java.util.Scanner;
+
+public class N1077 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int n = 0;
+
+        while(n<=a){
+            System.out.println(n);
+            n++;
+        }
+    }
+}
