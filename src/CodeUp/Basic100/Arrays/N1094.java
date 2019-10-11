5f82d0809a73884cc6a8d08939a20e2ada73d213
sojeongw <redthink90@gmail.com>
2019-10-11 20:06:19 +0900

    [CodeUp] 배열

---
+++ src/CodeUp/Basic100/Arrays/N1094.java
@@ -0,0 +1,17 @@
+package CodeUp.Basic100.Arrays;
+
+import java.util.Scanner;
+
+public class N1094 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+        for(int i=arr.length-1; i>=0; i--){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
--- src/CodeUp/Basic100/Problems/N1091.java
+++ src/CodeUp/Basic100/Problems/N1091.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class N1091 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
+        long a = sc.nextInt();
         int m = sc.nextInt();
         int d = sc.nextInt();
         int n = sc.nextInt();
 
-        for(int i=1; i<=n; i++){
+        for(int i=1; i<n; i++){
             a = a*m + d;
         }
         System.out.println(a);
