0b0127749229d3b53f8ec28f05a04362bb484814
sojeongw <redthink90@gmail.com>
2019-10-13 23:18:56 +0900

    [CodeUp] 배열

---
+++ src/CodeUp/Basic100/Arrays/N1098.java
@@ -0,0 +1,19 @@
+package CodeUp.Basic100.Arrays;
+
+import java.util.Scanner;
+
+public class N1098 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int h = sc.nextInt();
+        int w = sc.nextInt();
+
+        int n = sc.nextInt();
+
+        int l = sc.nextInt();
+        int d = sc.nextInt();
+
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+    }
+}
