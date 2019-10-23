e3104534340f24e2715cfaeb66cf776d6d9cbfec
sojeongw <redthink90@gmail.com>
2019-10-23 17:51:50 +0900

    [BOJ] 1차원 배열

---
+++ src/BaekjoonOnlineJudge/StepByStep/OneDimensionalArray/TheNumber_2577.java
@@ -0,0 +1,28 @@
+package BaekjoonOnlineJudge.StepByStep.OneDimensionalArray;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class TheNumber_2577 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+        br.close();
+
+        String abc = Integer.toString(a * b * c);
+        int[] count = new int[10];
+
+        for(int i=0; i<abc.length(); i++){
+            // char를 int로 변환하는 법
+            // abc의 각 자리값이 결국 count의 index라는 게 포인트
+            count[abc.charAt(i) - '0']++;
+        }
+
+        for(int i=0; i<count.length; i++){
+            System.out.println(count[i]);
+        }
+    }
+}
