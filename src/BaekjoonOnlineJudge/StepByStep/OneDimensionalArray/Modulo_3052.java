317fb2ace9fbaeff2429fb67b4aa93a6785b1176
sojeongw <redthink90@gmail.com>
2019-10-23 17:51:50 +0900

    [BOJ] 1차원 배열

---
+++ src/BaekjoonOnlineJudge/StepByStep/OneDimensionalArray/Modulo_3052.java
@@ -0,0 +1,20 @@
+package BaekjoonOnlineJudge.StepByStep.OneDimensionalArray;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Modulo_3052 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = new int[10];
+
+        for(int i=0; i<10; i++){
+            arr[i] = Integer.parseInt(br.readLine())%42;
+        }
+
+        int count = 0;
+        for(int i=0; i<arr.length; i++){
+        }
+    }
+}
