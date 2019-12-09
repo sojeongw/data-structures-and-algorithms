89664021f9c1a720b2879e0e0bc193dcf5ef0368
sojeongw <redthink90@gmail.com>
2019-12-09 15:03:15 +0900

    [BOJ] 함수: 정수 N개의 합

---
+++ src/BaekjoonOnlineJudge/StepByStep/Function/SumOfIntegers_15596.java
@@ -0,0 +1,27 @@
+package BaekjoonOnlineJudge.StepByStep.Function;
+
+import java.util.Scanner;
+
+public class SumOfIntegers_15596 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+
+        for(int i=0; i<n; i++){
+            a[i] = sc.nextInt();
+        }
+
+        Test t = new Test();
+        t.sum(a);
+    }
+}
+class Test{
+    public long sum(int[] a){
+        long sum = 0;
+        for(int i=0; i<a.length; i++){
+            sum += a[i];
+        }
+        return sum;
+    }
+}
