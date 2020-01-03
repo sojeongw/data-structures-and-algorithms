6cf80f8add316916138713360ff03e623209498d
sojeongw <redthink90@gmail.com>
2020-01-03 17:05:50 +0900

    [BOJ] 재귀: 팩토리얼

---
+++ src/BaekjoonOnlineJudge/StepByStep/Recursion/Factorial_10872.java
@@ -0,0 +1,24 @@
+package BaekjoonOnlineJudge.StepByStep.Recursion;
+
+import java.io.*;
+
+public class Factorial_10872 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int input = Integer.parseInt(br.readLine());
+        br.close();
+
+        bw.write(String.valueOf(calFac(input)));
+        bw.close();
+    }
+
+    static int calFac(int input){
+        if(input==1 || input==0){
+            return 1;
+        }else{
+            return input * calFac(input-1);
+        }
+    }
+}
