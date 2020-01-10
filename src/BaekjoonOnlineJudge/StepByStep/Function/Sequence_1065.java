15744090050a38794a667fe90aa5c3dac68236e4
sojeongw <redthink90@gmail.com>
2020-01-10 16:23:13 +0900

    [BOJ] 함수: 한수(In Progress)

---
+++ src/BaekjoonOnlineJudge/StepByStep/Function/Sequence_1065.java
@@ -0,0 +1,28 @@
+package BaekjoonOnlineJudge.StepByStep.Function;
+
+import java.io.*;
+
+public class Sequence_1065 {
+    int range = 1000;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i=1; i<n; i++){
+            calSeqeunce(i);
+        }
+
+    }
+
+    static void calSeqeunce(int input){
+        int[] result = new int[range];
+
+        while(input != 0){
+
+        }
+
+
+    }
+}
