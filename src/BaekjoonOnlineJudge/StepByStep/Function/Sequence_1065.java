28d9df190889a9de01d537a0062ed23074869e02
sojeongw <redthink90@gmail.com>
2020-01-11 16:24:27 +0900

    [BOJ] 함수: 한수

--- src/BaekjoonOnlineJudge/StepByStep/Function/Sequence_1065.java
+++ src/BaekjoonOnlineJudge/StepByStep/Function/Sequence_1065.java
@@ -3,26 +3,34 @@ package BaekjoonOnlineJudge.StepByStep.Function;
 import java.io.*;
 
 public class Sequence_1065 {
-    int range = 1000;
+    static int count = 0;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
 
-        for(int i=1; i<n; i++){
-            calSeqeunce(i);
+        // 어차피 두 자리 수 까지는 다 한수이다.
+        if (n < 100) {
+            count = n;
+        } else {
+            count = 99;
+            for (int i = 100; i <= n; i++) {
+                calSequence(i);
+            }
         }
-
+        bw.write(String.valueOf(count));
+        bw.close();
     }
 
-    static void calSeqeunce(int input){
-        int[] result = new int[range];
+    static void calSequence(int n) {
+        int a = n / 100;
+        int b= (n / 10) % 10;
+        int c = n % 10;
 
-        while(input != 0){
-
+        if (a-b == b-c) {
+            count++;
         }
-
-
     }
 }
