66bb426c1869102dc3959d1fd632d23e6cf0cc44
dodeon <sojeongw.official@gmail.com>
2021-10-31 21:07:51 +0900

    [ndb] 4-3. 구현: 게임 개발

--- python/ndb/implementation/ch4-3_answer.py
+++ python/ndb/implementation/ch4-3_answer.py
@@ -1,73 +1,73 @@
+# 게임 맵 사이즈인 n, m을 입력받는다.
 n, m = map(int, input().split())
 
-# 방문한 위치를 저장하기 위한 맵을 생성해 0으로 초기화한다.
+# 방문한 위치를 저장하기 위한 맵을 생성한 뒤, 0으로 초기화한다.
+# 파이썬에서 2차원 리스트를 만들 때는 이처럼 컴프리헨션 문법을 사용하면 좋다.
 d = [[0] * m for _ in range(n)]
 
-# 현재 캐릭터의 X, Y, 방향을 입력받는다.
+# 캐릭터의 현재 좌표와 방향을 입력받는다.
 x, y, direction = map(int, input().split())
-
-# 현재 좌표에 방문했음을 체크한다.
+# 현재 있는 좌표는 방문 처리 한다.
 d[x][y] = 1
 
-# 전체 맵 정보를 입력받는다.
+# 게임 맵 정보를 입력 받는다.
 array = []
 for i in range(n):
-    array.append(list(map(int, input().split())))
+  array.append(list(map(int, input().split())))
 
 # 북, 동, 남, 서 방향을 정의한다.
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
 
-
-# 왼쪽으로 회전하는 함수를 만든다.
+# 왼쪽으로 회전하는 기능을 정의한다.
 def turn_left():
-    global direction
-    direction -= 1
+  # direction 변수가 함수 밖에서 선언된 전역 변수이므로 global을 붙여준다.
+  global direction
+  direction -= 1
 
-    if direction == -1:
-        direction = 3
-
+  if direction == -1:
+    direction = 3
 
 # 시뮬레이션을 시작한다.
 count = 1
 turn_time = 0
 
 while True:
-    # 왼쪽으로 회전한다.
-    turn_left()
+  # 왼쪽으로 회전한다.
+  turn_left()
 
-    nx = x + dx[direction]
-    ny = y + dy[direction]
+  nx = x + dx[direction]
+  ny = y + dx[direction]
 
-    # 회전한 이후 정면에 가지 않은 칸이 존재하면 이동한다.
-    if d[nx][ny] == 0 and array[nx][ny] == 0:
-        d[nx][ny] = 1
+  # 회전한 뒤 가본적 없는 칸이 앞에 있을 경우 이동한다.
+  if d[nx][ny] == 0 and array[nx][ny] == 0:
+    d[nx][ny] = 1
+    x = nx
+    y = ny
 
-        x = nx
-        y = ny
-        count += 1
-        turn_time = 0
+    count += 1
+    turn_time = 0
+    continue
 
-        continue
+  # 회전한 후 앞에 가본적 없는 칸이 없거나 바다라면 한 번 더 돈다.
+  else:
+    turn_time += 1
 
-    # 회전한 이후 정면에 가보지 않은 칸이 없거나 바다인 경우
+  # 네 방향 모두 갈 수 없다면
+  if turn_time == 4:
+    # 한 칸 뒤로 갈 수 있는지 확인하기 위해 좌표를 구하고
+    nx = x - dx[direction]
+    ny = y - dy[direction]
+
+    # 뒤로 가는 게 가능하다면 방향은 유지한 채로 한 칸 뒤로 간다.
+    if array[nx][ny] == 0:
+      x = nx
+      y = ny
+
+    # 뒤가 바다로 막혀있다면 움직임을 멈춘다.
     else:
-        turn_time += 1
+      break
 
-    # 네 방향 모두 갈 수 없는 경우
-    if turn_time == 4:
-        nx = x - dx[direction]
-        ny = y- dy[direction]
+    turn_time = 0
 
-        # 뒤로 갈 수 없다면 이동한다.
-        if array[nx][ny] == 0:
-            x = nx
-            y = ny
-
-        # 뒤가 바다로 막혀있는 경우
-        else:
-            break
-        turn_time = 0
-
-# 정답 출력
 print(count)
