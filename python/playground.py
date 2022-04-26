88c821747f8b7ea9828f3ef16a3f828a8032820d
sojeongw <sojeongw.official@gmail.com>
2022-04-27 07:12:43 +0900

    [ndb] DFS/BFS: 7-2. 부품 찾기

--- python/playground.py
+++ python/playground.py
@@ -1,8 +1,31 @@
-s = "chopper is not a tanuki"
-k = 5
+# 5
+# 8 3 7 9 2
+# 3
+# 5 7 9
 
-s = s.split()
-print(s)
-result = ' '.join(s[:k])
+n = int(input())
+numbers = sorted(list(map(int, input().split())))
+m = int(input())
+requests = list(map(int, input().split()))
 
-print(result)
+
+def binary_search(array, target, start, end):
+    if start > end:
+        return None
+    mid = (start + end) // 2
+
+    while start <= end:
+        if array[mid] == target:
+            return mid
+        elif array[mid] < target:
+            return binary_search(array, target, mid + 1, end)
+        else:
+            return binary_search(array, target, start, mid - 1)
+
+
+for request in requests:
+    result = binary_search(numbers, request, 0, n-1)
+    if result:
+        print('yes')
+    else:
+        print('no')
